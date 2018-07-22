2861984dd5cf9355a642f8a78ac1ce6c20f92a61
ri382j <ri382j@intl.att.com>
2018-07-22 16:43:29 +0300

    upgrade to ng6

--- src/master/app/core/core.module.ts
+++ src/master/app/core/core.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 import {MoviesService} from "./movies.service";
 import {LoggerService} from "./logger.service";
 import {ConfigService} from "./config.service";
-import {HttpModule} from "@angular/http";
+import {HttpClientModule} from "@angular/common/http";
 import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {MoviesDb} from "../data/movies.data";
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule,
+    HttpClientModule,
     InMemoryWebApiModule.forRoot(MoviesDb)
   ],
-  exports:[HttpModule],
+  exports:[HttpClientModule],
   providers:[MoviesService, LoggerService, ConfigService],
   declarations: []
 })
